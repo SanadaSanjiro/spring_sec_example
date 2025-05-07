39e42349a30c1b53832ded5d9a84088a49ab759a
SanadaSanjiro <trollegon@narod.ru>
2025-05-07 17:26:28 +0800

    Unused dependencies have been removed from the Role class

--- src/main/java/form/login/example/entity/MyRole.java
+++ src/main/java/form/login/example/entity/MyRole.java
@@ -5,12 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Простой класс роли. Содержит id, название роли (title)
